ec07e27763f23d59067f58e35a8a2c2aa55550d8
MarceloSpagnuolo <lu4huf@gmail.com>
2022-08-26 14:48:37 -0300

    contact confirm

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
 import email from "../../assets/images/email.png";
 import instagram from "../../assets/images/instagram.png";
 import facebook from "../../assets/images/facebook.png";
 import twitter from "../../assets/images/twitter.png";
+import check from "../../assets/images/check.png";
 
 const Contact = (): JSX.Element => {
+  const [show, setShow] = useState(true);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault(e);
+    setShow(!show);
+  };
+
   return (
     <div className="contact-container" id="contact">
       <div className="contact-content">
@@ -33,13 +41,35 @@ const Contact = (): JSX.Element => {
             </div>
           </div>
         </div>
-        <div className="contact-right">
-          <form action="" className="contact-form">
+        <div className={show ? "contact-right" : "contact-right none"}>
+          <form
+            action=""
+            className="contact-form"
+            onSubmit={e => handleSubmit(e)}
+          >
             <input type="text" name="name" placeholder="Name" />
             <input type="email" name="email" placeholder="Email" />
             <textarea name="message" rows={9} placeholder="Message"></textarea>
             <input type="submit" value="Send" />
           </form>
+        </div>
+        <div
+          className={
+            show ? "contact-right-confirm none" : "contact-right-confirm"
+          }
+        >
+          <div className="check">
+            <img src={check} alt="check" />
+          </div>
+          <div className="message-1">
+            <span>Thanks! Your message has been sent.</span>
+          </div>
+          <div className="message-2">
+            <span>We will contact you shortly.</span>
+          </div>
+          <div className="message-3">
+            <span>Send another message</span>
+          </div>
         </div>
       </div>
     </div>
